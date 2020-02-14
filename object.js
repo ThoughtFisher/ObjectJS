4c23aae33b692f06c49d2db9dae5068cd67d3c5f
jaime <jaime.molina@mattersupply.co>
2020-02-13 21:51:08 -0500

    object explanation. searching and arrays

---
+++ object.js
@@ -0,0 +1,32 @@
+var library = [
+  {
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true,
+    popularity: 4
+  },
+  {
+    author: 'Steve Jobs',
+    title: 'Walter Isaacson',
+    readingStatus: true,
+    popularity: 2
+  },
+  {
+    author: 'Suzanne Collins',
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    readingStatus: false,
+    popularity: 1
+  }
+]
+
+// Encontrar una propiedad del arreglo
+// para nuestro caso será el que tenga reading status TRUE
+
+for (var i = 0; i < library.length; i++) {
+  var book = "'" + library[i].title + "'" + ' by ' + library[i].author + '.'
+  if (library[i].readingStatus) {
+    console.log('Already read ' + book)
+  } else {
+    console.log('You still need to read ' + book)
+  }
+}
