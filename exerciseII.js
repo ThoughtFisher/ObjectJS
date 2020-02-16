3e81a6c758a47976fb108c47313d6abfeb1a9f17
jaime <jaime.molina@mattersupply.co>
2020-02-15 22:26:58 -0500

    exercise obj II

--- exerciseII.js
+++ exerciseII.js
@@ -29,3 +29,12 @@ var library = [
  * HINT: Iterar el arreglo y preguntar por la propiedad
  *       correcta
  */
+
+for (var i = 0; i < library.length; i++) {
+  var book = "'" + library[i].title + "'" + ' by ' + library[i].author + '.'
+  if (library[i].readingStatus) {
+    console.log('Already read ' + book)
+  } else {
+    console.log('You still need to read ' + book)
+  }
+}
