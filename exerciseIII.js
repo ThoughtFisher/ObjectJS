fe8de7b35e7229f28c10a7486ce743da3bdb293a
jaime <jaime.molina@mattersupply.co>
2020-02-18 17:41:38 -0500

    ejercicio III

---
+++ exerciseIII.js
@@ -0,0 +1,25 @@
+/**
+ * Imprimir llave valor de todo el objeto
+ * ¿Como se puede hacer con recursividad?
+ * ¿Como se puede hacer con iteración?
+ *
+ * HINT:
+ * 1) Typos en JS
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+ *
+ */
+const obj = {
+  a: 4,
+  b: 3,
+  c: {
+    one: 'one',
+    two: 'two',
+    three: {
+      key: [3, 4]
+    }
+  },
+  d: 1,
+  z: {
+    zKey: 'zValue'
+  }
+}
